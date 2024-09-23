9b47f028c4212337456b0b2cf3ca9f02ca60f906
Anuja-js <anujajs2002@gmail.com>
2024-09-23 15:32:11 +0530

    h

--- lib/home.dart
+++ lib/home.dart
@@ -4,8 +4,7 @@ const s=SizedBox;
 const colorred=Colors.red;
 const red=Colors.red;
 const yellow=Colors.yellow;
-
-
+String a="Anu";
 const black=Colors.black;
 const white=Colors.white;
 const blue=Colors.blue;
